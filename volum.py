5aa7d6f0ec3d3865cb1b5e4d107c3f4afc468ca6
tuliocarvalh <tuliocarvalh@gmail.com>
2022-12-15 13:51:02 -0300

    First codes

---
+++ volum.py
@@ -0,0 +1,12 @@
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+devices = AudioUtilities.GetSpeakers()
+volum = 10.0
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volume.GetMute()
+volume.GetMasterVolumeLevel()
+volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(-volum, None)
